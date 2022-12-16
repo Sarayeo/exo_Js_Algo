6116eb4b49f04d464491bca091281ca7417fea91
Sarayeo <yeokpeusseusarafiela@gmail.com>
2022-12-16 23:12:44 +0100

    r

---
+++ tueur.js
@@ -0,0 +1,41 @@
+class chevalier {
+    constructor(nom,arme,armure,pv) {
+        this.nom = nom;
+        this.arme = arme;
+        this.defense = armure;
+        this.pv = pv;
+    }
+    let Attackluck = true
+    attack() {
+        if(armure){
+            Attackluck = 0.7
+        }else{
+            Attackluck = 0.5
+        }
+    }
+    attackHeracles(ennemie){
+            let dega = 1;
+        }
+}
+
+const Heracles = new chevalier("Jean", "","",10);
+
+/*class victimes {
+    constructor( nom , armure, arme, pv) {
+        this.nom = nom;
+        this.chancemourir = chancemourir;
+        this.chancedegat = chancedegat;
+        this.chancel2 = chancel2;
+    }
+}
+const PL = new victimes("Pierre-louis","" ,"", "", "");*/
+
+let Pe = 0.1
+while (chevalier.pv > 0 ) {
+    let pve = 3
+    if (chevalier.armure){
+        let deg = 1
+        Heracles deg
+    }
+
+}
